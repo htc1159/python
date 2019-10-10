c65286e167edc5e138c468ad9ef734632196267f
htc <htc@htcdeMacBook-Air.local>
2019-10-10 23:26:26 +0800

    set

--- d07/set.py
+++ d07/set.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-print(1)
+# set 是无序的唯一的
+s = set(['a','b','c'])
+print(s)
+
+# set中添加元素
+s.add('4')
+
+print(s)
+
+# set中移除元素
+s.remove('b')
+
+print(s)
