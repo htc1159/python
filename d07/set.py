d82431f7d76ae3c6626825d5424de138017be3de
htc <532302204@qq.com>
2019-10-11 07:00:36 +0000

    不可变对象分析

--- d07/set.py
+++ d07/set.py
@@ -13,3 +13,39 @@ print(s)
 s.remove('b')
 
 print(s)
+
+# 不可变对象
+
+a = ['c','b','a']
+
+a.sort()
+
+print(a)
+
+a = 'abc'
+
+b= a.replace('a','A')
+
+print(a)
+
+print(b)
+
+t1 = (1,2,3)
+
+t2 = (1,[2,3])
+
+d = {t1:1}
+
+print(d)
+# 由于tuple t2 中包含 list 而list 是可变的，所以无法计算出固定的hash值，不能作为 dict 的key
+#d[t2]=2
+
+#print(d)
+
+s= set([t1])
+
+print(s)
+# set he dict中的key都是需要计算hash值的，所以也不可以放入 set中
+#s.add(t2)
+
+#print(s)
